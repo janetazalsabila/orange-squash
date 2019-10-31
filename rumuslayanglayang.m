0eecdcf76eb2edc6347462ee5bf968aeb90127aa
Janeta Zalsabila <57222066+janetazalsabila@users.noreply.github.com>
2019-10-31 20:36:36 +0700

    Add files via upload

---
+++ rumuslayanglayang.m
@@ -0,0 +1,9 @@
+disp("Rumus Menghitung Luas dan Keliling Layang-Layang")
+
+diagonal1 = 20
+diagonal2 = 10
+panjang = 15
+lebar = 5
+
+luasLayanglayang = (diagonal1*diagonal2)/2
+kelilingLayanglayang = (panjang+lebar)*2
