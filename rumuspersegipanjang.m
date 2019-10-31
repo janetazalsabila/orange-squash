16ad237b28c3faf290d56f49ff70bfb0b3639a99
Janeta Zalsabila <57222066+janetazalsabila@users.noreply.github.com>
2019-10-31 20:17:06 +0700

    Add files via upload

---
+++ rumuspersegipanjang.m
@@ -0,0 +1,11 @@
+disp("Rumus Mencari Luas dan Keliling Persegi Panjang")
+
+panjang = 3
+lebar = 5
+
+rumusluasPersegiPanjang = panjang*lebar
+
+panjang = 5
+lebar = 3
+
+rumuskelilingPersegiPanjang = 2*(panjang*lebar)
