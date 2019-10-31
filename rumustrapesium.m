8c0e9f408fb3aa275d436e5eeac546a2d0c25a15
Janeta Zalsabila <57222066+janetazalsabila@users.noreply.github.com>
2019-10-31 20:21:22 +0700

    Add files via upload

---
+++ rumustrapesium.m
@@ -0,0 +1,10 @@
+disp("Rumus Menghitung Luas dan Keliling Trapesium")
+
+sisi1 = 15
+sisi2 =20
+sisi3 =14
+sisi4 =12
+tinggi = 12
+
+luasTrapesium = (sisi1+sisi2)*tinggi/2
+kelilingTrapesium = sisi1+sisi2+sisi3+sisi4
