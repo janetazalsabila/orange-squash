8c9efe4b48b48ebb66a737eac608746fe052fcd3
Janeta Zalsabila <57222066+janetazalsabila@users.noreply.github.com>
2019-10-31 20:15:06 +0700

    Add files via upload

---
+++ rumusjajargenjang.m
@@ -0,0 +1,9 @@
+disp("Rumus Menghitung Luas dan Keliling Jajar Genjang")
+
+alas = 10
+tinggi = 5
+luasJajargenjang = alas*tinggi
+
+panjang = 20
+lebar = 15
+kelilingJajargenjang = 2*(panjang+lebar)
