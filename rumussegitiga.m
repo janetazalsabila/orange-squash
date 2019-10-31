20dd7d783dbf0582c15f2130ac19a90142d6b05f
Janeta Zalsabila <57222066+janetazalsabila@users.noreply.github.com>
2019-10-31 20:08:54 +0700

    Add files via upload

---
+++ rumussegitiga.m
@@ -0,0 +1,12 @@
+disp("Rumus Menghitung Luas dan Keliling Segitiga")
+
+alas = 3
+tinggi = 5
+
+rumusluasSegitiga = (alas*tinggi)/2
+
+sisi1 = 5
+sisi2 = 4
+sisi3 = 3
+
+rumuskelilingSegitiga = sisi1 + sisi2 + sisi3
